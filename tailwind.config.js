60b22ba1c6b410af2bef49938122c758dabfdac5
gwynethtan <S10257317@connect.np.edu.sg>
2025-04-21 17:52:22 +0800

    build: Added magicui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,67 @@ const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite-react/**/*.js"
   ],
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'neon': '#C62EE8',
-        'secondary': '#818AA2',
-        'card':'rgba(217, 217, 217, 0.1)',
-        'purple-blur': '#350F55',
-        'blue-blur': '#292054',
-      }
-    },
+	screens: {
+		'sm': '640px',    // min-width: 640px
+		'md': '768px',    // min-width: 768px (iPad starts here)
+		'lg': '1301px',   // min-width: 1301px
+	  },
+  	extend: {
+  		colors: {
+  			transparent: 'transparent',
+  			current: 'currentColor',
+  			neon: '#C62EE8',
+  			secondary: '#818AA2',
+  			card: 'rgba(217,217,217,0.1)',
+  			nav: '#330F41',
+  			'purple-blur': '#350F55',
+  			'blue-blur': '#292054',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [flowbiteReact],
+  plugins: [flowbiteReact, require("tailwindcss-animate")],
 };
