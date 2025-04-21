e2d862fbbab6aa47923a1bdf6e9d0607087b6023
gwynethtan <S10257317@connect.np.edu.sg>
2025-04-21 18:06:54 +0800

    style: Redesigned the navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,39 +1,51 @@
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
-import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/sparkle.png"
+import ClickSpark from './ClickSpark';
+import "../index.css";
 
-const NavigationBar=()=> {
-  return (
-    <nav class="fixed w-full border-gray-200 bg-white top-0 h-16 z-50">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" class="text-neon">Services</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-            </li>
-          </ul>
+const NavigationBar=()=> { 
+  return(
+      <ClickSpark
+      sparkColor='#fff'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+      >
+      <nav className="bg-nav top-0 h-16 z-50 fixed w-screen">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between pt-4 md:mx-auto md:p-4">
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse ml-4">
+              <img src={Logo} className="h-8" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Gwyneth Tan</span>
+          </a>
+          <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center mr-4 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-solid-bg" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+          </button>
+          <div className="hidden w-screen md:block md:w-auto" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium bg-nav rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+              <li>
+              <NavLink to="/" className="block py-3 px-5 md:p-0 text-neon rounded-sm md:hover:bg-transparent md:border-0">Home</NavLink>            
+              </li>
+              <li>
+              <NavLink to="/Projects" className="block py-3 px-5 md:pl-10 md:p-0 text-neon rounded-sm md:hover:bg-transparent md:border-0">Projects</NavLink>            
+              </li>
+              <li>
+              <NavLink to="/Experiences" className="block py-3 px-5 md:pl-10 md:p-0 text-neon rounded-sm md:hover:bg-transparent md:border-0">Experiences</NavLink>            
+              </li>
+              <li>
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="block py-3 px-5 md:pl-10 md:p-0 text-neon rounded-sm md:hover:bg-transparent md:border-0">
+                  Resume
+                </a>              
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
+      </nav>
+  </ClickSpark>
+);
 }
 
 export default NavigationBar
