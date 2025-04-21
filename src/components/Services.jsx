23e6c9cb82c9ffa864aba84e57592858324ed8eb
gwynethtan <S10257317@connect.np.edu.sg>
2025-04-21 17:59:46 +0800

    feat: Added services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,51 @@
 import React from "react";
-import ServiceCard from './ServiceCard';  
 import Cube from "../assets/cube.png"
 import Code from "../assets/code.png"
 import Sparkle from "../assets/sparkle.png"
+import { motion } from "framer-motion";
 
+
+const serviceCards=[
+  {
+    title:"Multi Media Development",
+    icon: Cube, 
+    description:"Proficient in developing innovative solutions for AR/VR/MR and web platforms, leveraging immersive technologies for real-world impact."
+  },
+  {
+    title:"Software Development",
+    icon: Code, 
+    description:"Skilled in building web-based solutions using APIs, databases, libraries, and frameworks, with a focus on functionality, scalability, and user experience."
+  },  
+  {
+    title:"Design and Marketing",
+    icon: Sparkle, 
+    description:"Experience in marketing and UI/UX design, with skills in user research, brand alignment, and delivering client-focused solutions."
+  },
+]    
 const Services=()=> {
   return (
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-12 px-16">
-        <ServiceCard 
-        title="Multi Media Development" 
-        image={Cube} 
-        description="Experienced in building gamification systems and solutions for AR/VR/MR and web platforms, with expertise in both 3D and 2D art pipelines."
-        />
-        <ServiceCard 
-        title="Web Development" 
-        image={Code} 
-        description="I have experience in using APIs, databases, libraries and frameworks to build websites that creates solutions."
-        />
-        <ServiceCard 
-        title="Design and Marketing" 
-        image={Sparkle} 
-        description="I am skilled in marketing and UI/UX designing. I am able to do research and follow brand guides to meet client needs."
-        />
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 px-16 md:px-36 lg:px-16">
+      {
+        serviceCards.map((serviceCard,index)=>(
+          <article key={index}>
+            <motion.div
+            initial={{opacity:0, x:-70}}
+            whileInView={{opacity:1, x:0}}
+            viewport={{once:false}}
+            transition={{duration:2}}
+            >
+              <div className="rounded-2xl border-2 border-neon bg-card flex-col justify-items-center p-4 space-y-8">
+                <h2 className="block font-semibold text-lg text-white">{serviceCard.title}</h2>
+                <div className="filter drop-shadow-[0_0_24px_rgba(0,255,255,1)]">
+                <img src={serviceCard.icon} alt="Service Icon" className="w-14 h-14 "/>
+
+                </div>
+                <p className="block text-secondary text-sm font-medium text-justify break-words">{serviceCard.description}</p>
+              </div>
+            </motion.div>
+        </article>
+        ))
+      }
     </div>
   );
 }
