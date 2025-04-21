14c95330bf75b0bc5ee4bcd8f3107058754899b1
gwynethtan <S10257317@connect.np.edu.sg>
2025-04-21 18:06:24 +0800

    feat: Allows navigation and routing to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import NavigationBar from "./components/NavigationBar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-import Threads from "./components/Threads";
-
+import ScrollToTop from './components/ScrollToTop';
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Experiences from "./pages/Experiences"
+import Layout from "./Layout"
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 export default function App() {
   return (
-    <>
-      <NavigationBar />
-      <Hero />
-      <Services />
-      <div style={{ width: '100%', height: '300px', position: 'relative' }}>
-        <Threads
-          amplitude={2}
-          distance={0}
-          enableMouseInteraction={true}
-        />
-      </div>
-    </>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
+          <Route path="/Experiences" element={<Experiences/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
