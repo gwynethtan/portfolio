fcbf9fb34075598e068ff3b4aeb521db30b9f8df
gwynethtan <S10257317@connect.np.edu.sg>
2025-05-23 10:30:14 +0800

    style: Change styling for project preview

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
-import RollingGallery from './RollingGallery';
 import NavLinkButton from './NavLinkButton'; 
+import ProjectPreviewPic from '../assets/projectPreview.png'
 
 const ProjectPreview=()=>{
     return(
@@ -13,11 +13,13 @@ const ProjectPreview=()=>{
             transition={{duration:1}}
             className=" w-screen"
             >
-                <div className="flex flex-col items-center">
-                    <h2 className="text-neon font-semibold text-4xl sm:text-5xl">Projects</h2>
-                    <RollingGallery autoplay={true} pauseOnHover={true} />
-                    <NavLinkButton text="View Projects" link="Projects"/>
-                </div>
+                <div className="flex flex-col lg:flex-row items-center">
+                    <div>
+                        <h2 className="text-neon font-semibold text-4xl sm:text-5xl">Projects</h2>
+                        <NavLinkButton text="View Projects" link="Projects"/>
+                    </div>
+                    <img src={ProjectPreviewPic} className="mr-3" />
+                    </div>
             </motion.div>
         </section>
     );
